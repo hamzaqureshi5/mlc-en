32785012f5306db13bf1a0600bd261f27349435b
Hamza Qureshi <hamza7771.861@gmail.com>
2025-07-10 12:45:22 +0500

    Version 0.0.1

--- chapter_gpu_acceleration/index.md
+++
@@ -1,8 +0,0 @@
-# GPU and Hardware Acceleration
-
-```toc
-:maxdepth: 2
-
-part1
-part2
-```
--- mlc/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Saved source code for "Machine Learning Compiler" (https://mlc.ai).
-
-"""
-
-__version__ = "0.0.1"
